e0df02d9b7f666eeaf2c374d7f21ec7854681bf3
Phenrril <matias.orset@hotmail.com>
2026-04-09 22:01:29 -0300

    changes2

--- src/includes/header.php
+++ src/includes/header.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Armazón | Panel</title>
+    <title>ArgenOptik | Panel</title>
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
